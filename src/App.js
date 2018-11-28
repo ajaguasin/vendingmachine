ca508353f95dba10d94ac37e94034ecc39dd4940
ajaguasin <ajaguasin@gmail.com>
2018-11-28 15:15:13 -0800

    Refactor UI

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import "./App.css";
 import VendingMachine from "./classes/vendingMachine/VendingMachine.js";
 import Slot from "./components/Slot";
-import { FaBeer } from "react-icons/fa";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 
 class App extends Component {
   constructor() {
@@ -10,15 +11,30 @@ class App extends Component {
     this.VendingMachine = new VendingMachine();
     this.state = {
       register: this.VendingMachine.vendingRegister,
-      inventoryOutput: ""
+      inventoryOutput: this.formatInventoryOutput()
     };
   }
+
+  formatInventoryOutput = () => {
+    let obj = {};
+
+    JSON.parse(this.VendingMachine.printInventory()).map(e => {
+      obj[`${e.slotName}`] = e.slotQuantity.length;
+    });
+
+    return obj;
+  };
   render() {
     const register = this.VendingMachine.vendingRegister;
     console.log(this.state);
     return (
       <div className="app">
-        <div className="vendingMachine">
+        <Grid
+          className="vendingMachine"
+          container={true}
+          direction={"row"}
+          // wrap={"wrap"}
+        >
           {this.VendingMachine.inventory.items.map((item, index) => {
             return (
               <Slot
@@ -28,45 +44,43 @@ class App extends Component {
               />
             );
           })}
-        </div>
+        </Grid>
 
-        <div className="controls">
+        <Grid container={true} className="controls">
           <div className="moneyButtons">
             {Object.entries(register.customerInput).map((el, index) => {
               return (
-                <button
+                <Button
                   key={index}
+                  variant="contained"
+                  color="default"
                   onClick={() => {
                     this.VendingMachine.inputMoney(el[0]);
                     this.setState({
                       register: this.state.register
                     });
                   }}
-                >{`Input ${el[0]}`}</button>
+                >{`Input ${el[0]}`}</Button>
               );
             })}
           </div>
-          <div>
-            <button>Input Code</button>
-            <button>Get Change</button>
-            <button
-              onClick={() => {
-                this.setState({
-                  inventoryOutput: this.VendingMachine.printInventory()
-                });
-              }}
-            >
-              Show Inventory
-            </button>
-          </div>
-          <div className="screen">
+          <Grid item={true}>
+            <Button variant="contained" color="default">
+              Refill Inventory
+            </Button>
+
+            <Button variant="contained" color="default">
+              Get Change
+            </Button>
+          </Grid>
+          <Grid item={true} xs={12} md={12} lg={12} className="screen">
             <p className="moneyInput">
               {`You've inputted: $${this.state.register.customerTotal / 100}`}
             </p>
             <p className="change" />
-            <p className="inventory">{this.state.inventoryOutput}</p>
-          </div>
-        </div>
+            <p>{this.state.inventoryOutput["Cheetos"]}</p>
+          </Grid>
+        </Grid>
       </div>
     );
   }
--- src/components/Slot.js
+++ src/components/Slot.js
@@ -1,20 +1,31 @@
 import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
 
 const Slot = ({ item, dispenseSnack }) => {
   return (
-    <div className="slot">
-      <div>
-        {item.slotName}: ${item.price / 100}
-      </div>
-      <div>Quantity: {item.slotQuantity.length}</div>
-      <button
-        onClick={() => {
-          dispenseSnack(item.gridInput);
-        }}
-      >
-        {item.gridCoord}
-      </button>
-    </div>
+    <Grid item={true} xs={4} md={4} lg={4}>
+      <Card className="slot">
+        <CardContent>
+          <Typography>
+            {item.slotName}: ${item.price / 100}
+          </Typography>
+          <Typography>Quantity: {item.slotQuantity.length}</Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              dispenseSnack(item.gridInput);
+            }}
+          >
+            {item.gridCoord}
+          </Button>
+        </CardContent>
+      </Card>
+    </Grid>
   );
 };
 
